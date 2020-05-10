a403683f73f068b6a2a0e735ba3ed73410dfc6c7
Aditya Loshali <adityaloshali1@gmail.com>
2020-05-10 09:19:39 +0530

    dockered

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+  console.log('env', process.env.NODE_ENV)
+  console.log('port', process.env.PORT)
+
   await app.listen(process.env.PORT || port);
 }
 bootstrap();
