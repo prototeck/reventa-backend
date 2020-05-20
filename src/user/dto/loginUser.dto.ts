c2c8f90dc9ad54bb29ed9339c17112e5f9321098
Aditya Loshali <adityaloshali1@gmail.com>
2020-05-20 22:35:49 +0530

    save up

--- src/user/dto/loginUser.dto.ts
+++ src/user/dto/loginUser.dto.ts
@@ -1,6 +1,6 @@
 import { ObjectType, Field, ID, Float } from '@nestjs/graphql';
 
-@ObjectType('User', { description: 'The User model' })
+@ObjectType('UserLog', { description: 'The User model' })
 export class LoginUserDTO {
 
     @Field(() => ID)
